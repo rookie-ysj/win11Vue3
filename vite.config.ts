05334807e3ed7a978bc5eb98dc9a99272d25e9d1
rookie-ysj <ysj15148242832@gmail.com>
2025-02-09 20:29:57 +0800

    配置@

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 设置 `@` 指向 `src` 目录
+      // 这里可以继续添加其他别名
+      // '/comps/': path.resolve(__dirname, './src/components'),
+    },
+  },
 })
