18736c1150c76000669cd9f01d866baebbd37db8
rookie-ysj <ysj15148242832@gmail.com>
2025-02-22 16:35:22 +0800

    lock-screen style

--- src/views/lockScreen/Index.vue
+++ src/views/lockScreen/Index.vue
@@ -5,19 +5,26 @@ import dayjs from 'dayjs'
 
 const currentDay = dayjs()
 const globalSetting = useGlobalSetting()
+
+const time = currentDay.format('h:mm A')
 </script>
 
 <template>
-  <div class="lock-screen flex" @click="() => globalSetting.setLock(false)">
-    <div>{{ currentDay.format() }}</div>
+  <div class="lock-screen" @click="() => globalSetting.setLock(false)">
+    <div class="text-5xl mt-36 mb-1">
+      {{ time }}
+    </div>
     <div>{{ `${currentDay.month() + 1}月${currentDay.date()}日${getDayOfWeek(currentDay.day())}` }}</div>
   </div>
 </template>
 
 <style scoped>
 .lock-screen {
+  display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
+  text-align: center;
   background-image: url("@/assets/img/lock-screen.jpg");
   position: fixed;
   top: 0;
