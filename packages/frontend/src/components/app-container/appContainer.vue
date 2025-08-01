078e6b9fbb9a9d33cffef830e0e87cd82bcd7683
yangshengjie <shengjie_yang@hmglog.com>
2025-08-01 17:06:32 +0800

    feat(frontend/appContainer): 拖拽

---
+++ packages/frontend/src/components/app-container/appContainer.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { useTemplateRef } from 'vue';
+import { useDraggable } from '@vueuse/core';
+
+const header = useTemplateRef<HTMLDivElement>('header');
+
+useDraggable(header, {
+  initialValue: {
+    x: 0,
+    y: 0,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <div ref="header" class="flex">
+      <div>
+        <slot name="title" />
+      </div>
+      <div class="flex justify-around">
+        <div>最小化</div>
+        <div>最大化</div>
+        <div>关闭</div>
+      </div>
+    </div>
+    <slot name="default" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
