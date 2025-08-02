7c1083e90aeff46cda1a54de190d6d6943f1b636
rookie-ysj <ysj15148242832@gmail.com>
2025-08-02 23:28:50 +0800

    feat(toolbar): 优化 - edge

--- packages/frontend/src/components/tool-bar/toolBar.vue
+++ packages/frontend/src/components/tool-bar/toolBar.vue
@@ -29,55 +29,26 @@ const { style } = useDraggable(header, {
 
 <template>
   <div class="fixed w-full h-full" :style="style">
-    <div
-        ref="header"
-        class="flex justify-between toolbar"
-        :style="{ background: bg, width: '100vw' }"
-    >
+    <div ref="header" class="flex justify-between toolbar" :style="{ background: bg, width: '100vw' }">
       <div class="flex text-xss items-center" :style="{
         color: invert ? 'white' : 'black'
       }">
-        <Icon
-            class="ml-2 mr-2"
-            :src="src"
-            width="14"
-        />
+        <Icon class="ml-2 mr-2" :src="src" width="14" />
         {{ name }}
       </div>
       <div class="flex justify-around toolbar__btns">
         <div @click="minimize">
-          <Icon
-              payload="mnmz"
-              pr
-              src="minimize"
-              ui
-              invert
-              width="12"
-          />
+          <Icon payload="mnmz" pr src="minimize" ui :invert="invert" width="12" />
         </div>
         <div @click="maximize">
-          <Icon
-              ui
-              pr
-              width="12"
-              payload="mxmz"
-              invert
-              :src="true ? 'maximize' : 'maxmin'"
-          />
+          <Icon ui pr width="12" payload="mxmz" :invert="invert" :src="true ? 'maximize' : 'maxmin'" />
         </div>
         <div @click="close" class="toolbar__closeBtn">
-          <Icon
-              invert
-              ui
-              pr
-              width="12"
-              payload="mxmz"
-              src="close"
-          />
+          <Icon :invert="invert" ui pr width="12" payload="mxmz" src="close" />
         </div>
       </div>
     </div>
-    <slot name="default"/>
+    <slot name="default" />
   </div>
 </template>
 
@@ -93,13 +64,13 @@ const { style } = useDraggable(header, {
       &:hover {
         background: rgba(136, 136, 136, 0.2);
       }
+    }
 
-      .toolbar__closeBtn:hover {
-        background: rgba(255, 0, 0, 0.8);
+    .toolbar__closeBtn:hover {
+      background: rgba(255, 0, 0, 0.8);
 
-        img {
-          filter: invert(1);
-        }
+      img {
+        filter: invert(1);
       }
     }
   }
--- packages/frontend/src/containers/applications/edge/edge.vue
+++ packages/frontend/src/containers/applications/edge/edge.vue
@@ -3,15 +3,21 @@ import ToolBar from '@/components/tool-bar/toolBar.vue'
 import { EDGE } from '@/constants'
 import { useApplication } from '@/store'
 
-const edgeApp = useApplication().applications[EDGE.name]
+const edgeName = EDGE.name
+const applicationStore = useApplication()
+const edgeApp = useApplication().applications[edgeName]
 </script>
 
 <template>
-  <div v-if="!edgeApp.hide">
-    <ToolBar
-        :icon="edgeApp.icon" name="edgeApp.name"
-    />
-  </div>
+  <transition enter-active-class="animate__animated animate__zoomInUp"
+    leave-active-class="animate__animated animate__zoomOutDown">
+    <ToolBar v-if="edgeApp.open" v-show="!edgeApp.hide" :name="edgeName" :src="EDGE.icon"
+      :close="() => applicationStore.toggleApplicationOpen(edgeName, false)" :maximize="() => { }"
+      :minimize="() => applicationStore.toggleApplicationHide(edgeName, true)">
+      <div class="w-full h-full bg-white">
+      </div>
+    </ToolBar>
+  </transition>
 </template>
 
 <style scoped>
